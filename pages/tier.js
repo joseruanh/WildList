9488e41328148e5099bbf684f125f64a1556f53b
joseruanh <joseruanh2@gmail.com>
2021-08-26 21:13:23 -0300

    adicionando tier

---
+++ pages/tier.js
@@ -0,0 +1,315 @@
+function  baron(){
+    var area = document.getElementById("res")
+
+    area.innerHTML = `
+
+
+    <div id="tierSS">
+                    
+                    <div id="list">
+                        S+ <br>
+                        
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/gragas.webp" alt="gragas"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/camille.webp" alt="camille"> </a>                      
+                    </div>
+                    <div id="list">
+                        <p>Classificação S+ são todos <br> aqueles que levam vantagem na maioria das matchups</p>
+                    <hr>
+                    <br>
+                    </div>
+                </div>
+
+
+                <div id="tierS">
+                    
+                        <div id="list">
+                            S <br>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/camille.webp" alt="camille"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/malphite.webp" alt="malphite"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/darius.webp" alt="darius"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/renekton.webp" alt="renek"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/garen.webp" alt="garen"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/akali.webp" alt="akali"> </a>
+                        </div>
+                        <div id="list">
+                            <p>Classificação S são todos <br> aqueles que levam vantagem na maioria das matchups</p>
+                            <hr>
+                        </div>    
+                </div>
+
+                <div id="tierA">
+                    <div id="list">
+                        <br>
+                        A <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/pantheon.webp" alt="pantheon"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/jax.webp" alt="jax"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/riven.webp" alt="riven"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/irelia.webp" alt="irelia"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/wukong.webp" alt="wukong"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/graves.webp" alt="graves"> </a>
+                    </div>
+                    <div id="list">
+                        <p>Classificação A são todos <br> aqueles que são boa escolha em boa parte das matchups</p>
+                        <hr>
+                    </div>
+    
+                </div>
+                <div id="tierB">
+                    <div id="list">
+                        <br>
+                        A <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/kennen.webp" alt="kennen"></a>
+                        
+                    </div>
+                    <div id="list">
+                        <p>Classificação B são todos <br> aqueles que não levam muita vantagem na lane</p>
+                        <hr>
+                    </div>
+    
+                </div>
+                <div id="tierC">
+                    <div id="list">
+                        <br>
+                        C <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/singed.webp" alt="singed"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/nasus.webp" alt="nasus"></a>
+
+                    </div>
+                    <div id="list">
+                        <p>Classificação C são todos <br> aqueles que não levam vantagem decisiva na lane</p>
+                        <hr>
+                    </div>
+                </div>
+
+                <div id="tierD">
+                    <div id="list">
+                        <br>
+                        D <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/teemo.webp" alt="teemo"></a>
+
+                    </div>
+                    <div id="list">
+                        <p>Classificação D são todos <br> aqueles que levam vantagem em muito poucas matchups</p>
+                        <hr>
+                    </div>
+                    
+    
+                </div>
+
+            </div>
+    
+    
+    
+    
+    `
+}
+
+function  jungle(){
+
+    var area = document.getElementById("res")
+
+    area.innerHTML = `
+    
+    <div id="tierSS">
+                    
+                    <div id="list">
+                        S+ <br>
+                        
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/lee-sin.webp" alt="Lee"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/khazix.webp" alt="KhaZix"> </a>                      
+                    </div>
+                    <div id="list">
+                        <p>Classificação S+ são todos <br> aqueles que levam vantagem na maioria das matchups</p>
+                    <hr>
+                    <br>
+                    </div>
+                </div>
+
+
+                <div id="tierS">
+                    
+                        <div id="list">
+                            S <br>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/camille.webp" alt="camille"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/wukong.webp" alt="wukong"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/master-yi.webp" alt="master"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/evelynn.webp" alt="evelynn"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/xin-zhao.webp" alt="xin-zhao"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/graves.webp" alt="graves"> </a>
+                        </div>
+                        <div id="list">
+                            <p>Classificação S são todos <br> aqueles que levam vantagem na maioria das matchups</p>
+                            <hr>
+                        </div>    
+                </div>
+
+                <div id="tierA">
+                    <div id="list">
+                        <br>
+                        A <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/olaf.webp" alt="olaf"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/shyvana.webp" alt="shyvana"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/rengar.webp" alt="rengar"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/vi.webp" alt="vi"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/jarvan-iv.webp" alt="jarvan"> </a>
+                    </div>
+                    <div id="list">
+                        <p>Classificação A são todos <br> aqueles que são boa escolha em boa parte das matchups</p>
+                        <hr>
+                    </div>
+    
+                </div>
+                <div id="tierB">
+                    <div id="list">
+                        <br>
+                        A <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/rammus.webp" alt="rammus"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/tryndamere.webp" alt="tryndamere"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/dr-mundo.webp" alt="dr mundo"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/pantheon.webp" alt="pantheon"></a>
+                        
+                    </div>
+                    <div id="list">
+                        <p>Classificação B são todos <br> aqueles que não levam muita vantagem na lane</p>
+                        <hr>
+                    </div>
+    
+                </div>
+                <div id="tierC">
+                    <div id="list">
+                        <br>
+                        C <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/amumu.webp" alt="amumu"></a>
+
+                    </div>
+                    <div id="list">
+                        <p>Classificação C são todos <br> aqueles que não levam vantagem decisiva na lane</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    `
+}
+
+function  mid(){
+
+
+    var area = document.getElementById("res")
+
+    area.innerHTML = `
+    
+    <div id="tierSS">
+                    
+                    <div id="list">
+                        S+ <br>
+                        
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/ziggs.webp" alt="ziggs"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/akali.webp" alt="akali"> </a>                      
+                    </div>
+                    <div id="list">
+                        <p>Classificação S+ são todos <br> aqueles que levam vantagem na maioria das matchups</p>
+                    <hr>
+                    <br>
+                    </div>
+                </div>
+
+
+                <div id="tierS">
+                    
+                        <div id="list">
+                            S <br>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/diana.webp" alt="diana"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/orianna.webp" alt="orianna"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/galio.webp" alt="galio"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/katarina.webp" alt="katarina"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/irelia.webp" alt="irelia"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/lucian.webp" alt="lucian"> </a>
+                            <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/corki.webp" alt="corki"> </a>
+                        </div>
+                        <div id="list">
+                            <p>Classificação S são todos <br> aqueles que levam vantagem na maioria das matchups</p>
+                            <hr>
+                        </div>    
+                </div>
+
+                <div id="tierA">
+                    <div id="list">
+                        <br>
+                        A <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/yasuo.webp" alt="yasuo"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/twisted-fate.webp" alt="tf"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/seraphine.webp" alt="seraphine"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/ahri.webp" alt="ahri"> </a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/zed.webp" alt="zed"> </a>
+                    </div>
+                    <div id="list">
+                        <p>Classificação A são todos <br> aqueles que são boa escolha em boa parte das matchups</p>
+                        <hr>
+                    </div>
+    
+                </div>
+                <div id="tierB">
+                    <div id="list">
+                        <br>
+                        A <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/aurelion-sol.webp" alt="Ao Sol"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/renekton.webp" alt="renek"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/akshan.webp" alt="akshan"></a>                        
+                    </div>
+                    <div id="list">
+                        <p>Classificação B são todos <br> aqueles que não levam muita vantagem na lane</p>
+                        <hr>
+                    </div>
+    
+                </div>
+                <div id="tierC">
+                    <div id="list">
+                        <br>
+                        C <br>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/annie.webp" alt="annie"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/fizz.webp" alt="fizz"></a>
+                        <a id="tierHeros" href="ahri.html"> <img id="imgGuia" src="../images/lux.webp" alt="lux"></a>
+
+                    </div>
+                    <div id="list">
+                        <p>Classificação C são todos <br> aqueles que não levam vantagem decisiva na lane</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    `
+}
+
+function  sup(){
+    window.alert("ainda irei adicionar")
+
+    var area = document.getElementById("res")
+
+    area.innerHTML = `Logo coloco`
+}
+
+function  adc(){
+    window.alert("ainda vou adicionar")
+
+    var area = document.getElementById("res")
+
+    area.innerHTML = `calma po`
+}
